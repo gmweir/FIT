76ae823c6fa082b8568bed05bb035efc868c0592
Gavin Weir <gawe@ipp-hgw.mpg.de>
2022-05-17 16:25:49 +0200

    no longer importing from fitting_scipy

--- __init__.py
+++ __init__.py
@@ -37,9 +37,19 @@ from . import fitting_dev as fitting # analysis:ignore
 from . import derivatives as derivatives # analysis:ignore
 from . import fitNL as fitNL
 
+# from . import fitting_np  # analysis:ignore
+# from .fitting_np import linreg as lin_reg   # analysis:ignore
+#
+# try:
+#     from . import fitting_scipy
+#     from .fitting_scipy import fit_curvefit
+# except ImportError:
+#     pass
+# # end try
+
 # ===================================================================== #
 from .derivatives import findiff1d, interp_profile, deriv_bsgaussian # analysis:ignore
-from .fitting_dev import linreg, savitzky_golay, spline, pchip, spline_bs # analysis:ignore
+from .fitting_dev import linreg, savitzky_golay, spline, pchip, spline_bs, fit_curvefit # analysis:ignore
 
 if __mpfitonpath__:
     from . import fitNL as fitNLm # analysis:ignore
